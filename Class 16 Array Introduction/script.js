74c038dfbe6806b0e6828fc8bbf781dcd330810e
Moinuddin-dotcom <moinchy7@gmail.com>
2024-07-15 11:30:56 +0600

    Arrat introduction

---
+++ Class 16 Array Introduction/script.js
@@ -0,0 +1,16 @@
+let marks_of_class_12 = [91, 58, 66, 88, 90, null, false, "Not Present"];
+
+console.log(marks_of_class_12);
+console.log(marks_of_class_12[0]);
+console.log(marks_of_class_12[1]);
+console.log(marks_of_class_12[2]);
+console.log(marks_of_class_12[3]);
+console.log(marks_of_class_12[4]);
+console.log(marks_of_class_12[5]);
+console.log(marks_of_class_12[6]);
+console.log(marks_of_class_12[7]);
+console.log(marks_of_class_12[8]);
+marks_of_class_12[9] = 99; // Adding a new value to the array
+marks_of_class_12[0] = 95; // Changing the value of an array
+console.log(marks_of_class_12);
+console.log(typeof marks_of_class_12);
