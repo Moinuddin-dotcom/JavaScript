739e856de0db26af586ad6bafa196d979c9f5e16
Moinuddin-dotcom <moinchy7@gmail.com>
2024-07-31 11:53:55 +0600

    practice five

--- Class 21 Practice 5/script.js
+++ Class 21 Practice 5/script.js
@@ -1,7 +1,6 @@
 
 // Excersise 5 - Problem 1
-const prompt = require("prompt-sync")(); //we install the prompt function on replit
-
+//const prompt = require("prompt-sync")(); //we install the prompt function on replit
 let arr = [1, 2, 3, 4, 5]; //Making a array
 let a = prompt("Enter your number: "); //activate prompt where a is a new array
 a = Number.parseInt(a); //converting string into a number
@@ -9,36 +8,36 @@ arr.push(a); //pushing inputed number in the end of array
 console.log(arr); //Finally printing it OR checking is my new number adding on my first array or not , the result is YES
 
 // Excersise 5 - Problem 2
-const prompt = require("prompt-sync")(); //we install the prompt function on replit
-let arr = [1, 2, 3, 4, 5]; //Making a array
-let a; //we keeping let a out of while loop because in while loop a is already checking the following command
+//const prompt = require("prompt-sync")(); //we install the prompt function on replit
+let arr1 = [1, 2, 3, 4, 5]; //Making a array
+let a1; //we keeping let a out of while loop because in while loop a is already checking the following command
 while (a != 0) {
-  a = prompt("Enter your number: "); //activate prompt where a is a new array
-  a = Number.parseInt(a); //converting string into a number
-  arr.push(a); //pushing inputed number in the end of array
+    a1 = prompt("Enter your number: "); //activate prompt where a is a new array
+    a1 = Number.parseInt(a1); //converting string into a number
+    arr.push(a1); //pushing inputed number in the end of array
 }
-console.log(arr); //Finally printing it OR checking is my new number adding on my first array or not , the result is YES
+console.log(arr1); //Finally printing it OR checking is my new number adding on my first array or not , the result is YES
 
 // Excersise 5 - Problem 3
-let arr = [50, 60, 70, 77, 66, 55];
-let a = arr.filter((e) => {
-  return e % 10 == 0;
+let arr2 = [50, 60, 70, 77, 66, 55];
+let a2 = arr2.filter((e) => {
+    return e % 10 == 0;
 });
-console.log(a);
+console.log(a2);
 
 // Excersise 5 - Problem 4
-let arr = [2, 4, 5, 6 , 30];
-let a = arr.map((v) => {
-  return v ** 2;
-  // OR
-  // return v * v;
+let arr4 = [2, 4, 5, 6, 30];
+let a4 = arr4.map((v) => {
+    return v ** 2;
+    // OR
+    // return v * v;
 });
-console.log(a);
+console.log(a4);
 
 // Excersise 5 - Problem 5
-let arr = [1, 2, 3, 4, 5];
+let arr5 = [1, 2, 3, 4, 5];
 const nw_func = (n1, n2) => {
-  return n1 * n2;
+    return n1 * n2;
 };
-let n = arr.reduce(nw_func);
+let n = arr5.reduce(nw_func);
 console.log(n);
