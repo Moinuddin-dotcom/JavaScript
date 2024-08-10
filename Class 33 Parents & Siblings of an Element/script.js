a21b27aae6d2f9a5e43f093e408e5f14128a39f1
Moinuddin-dotcom <moinchy7@gmail.com>
2024-08-10 16:42:12 +0600

    Parents & Siblings of an Element

---
+++ Class 33 Parents & Siblings of an Element/script.js
@@ -0,0 +1,5 @@
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode) // Returns any kind of parent node
+console.log(a.parentElement) // Returns valid html of parent element
+console.log(a.firstChild.nextSibling)
