9324519fa825ab181399f574f3d6feeebcdbf7c1
Moinuddin-dotcom <moinchy7@gmail.com>
2024-08-11 22:52:44 +0600

    element navigation

---
+++ Class 34 firstChild_firstElementChild/script.js
@@ -0,0 +1,8 @@
+const changeBgRed = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+// changeBgRed() //we need to comment out to unlock the function
+
+let a = document.body
+console.log(a.firstChild)
+console.log(a.firstElementChild)
