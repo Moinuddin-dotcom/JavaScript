6f68f630d0cb1e4cf158da4cc829d2cbf1152ed1
Moinuddin-dotcom <moinchy7@gmail.com>
2024-08-28 17:03:41 +0600

    beforebegin beforeend afterbegin afterend

---
+++ Class 44 beforebegin_end and afterbegin_end/script.js
@@ -0,0 +1,5 @@
+first.insertAdjacentHTML('beforebegin', '<div class="new">beforebegin</div>') //You have a div and (beforebegin) place his box outside top of manual div
+first.insertAdjacentHTML('beforeend', '<div class="new">beforeend</div>') //You have a div and (beforeend) place his box inside bottom of manual div
+first.insertAdjacentHTML('afterbegin', '<div class="new">afterbegin</div>') //You have a div and (afterbegin) place his box inside top of manual div
+first.insertAdjacentHTML('afterend', '<div class="new">afterend</div>') //You have a div and (afterend) place his box outside bottom of manual div
+
