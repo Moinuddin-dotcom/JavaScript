4dc55eaa7f0b43f19ffaafb356504a2fe70316f0
Moinuddin-dotcom <moinchy7@gmail.com>
2024-08-29 14:44:31 +0600

    className classList

---
+++ Class 45 className_classList/script.js
@@ -0,0 +1,6 @@
+first.className = "yellow text-dark"
+first.classList //to check the class list on browser console
+first.classList.remove("red")
+first.classList.add("red")
+first.classList.toggle("red") //if red is present then remove else add
+first.classList.contains("yellow")
