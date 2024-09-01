c8a3a27a26a5f6df18af09ceb90e7ecc4568fc09
Moinuddin-dotcom <moinchy7@gmail.com>
2024-09-01 11:24:08 +0600

    setTimeout setInterval

---
+++ Class 46 setTimeout_setInterval/script.js
@@ -0,0 +1,30 @@
+document.write("Hello")
+// //setTimeout
+// let a = setTimeout(function(){
+//   alert("i am inside settimeout")
+// },3000);
+// let b = prompt("Do you want to run the settimeout?")
+// if("n" == b){
+// clearTimeout(a)
+// }
+// console.log(a)
+// // Or
+// const x = (a, b) =>{
+//   console.log("setTimeout running" + (a + b))
+// }
+// setTimeout(x,2000,6,5)
+
+// //setInteval
+// let z = setInterval(function(){
+//   alert("I am inside setInteval")
+// },3000)
+// clearInterval(z)
+
+// // Practice task : Use setInterval function with argument
+// function smsbox (message, count){
+//   console.log(message , count)
+// }
+// function smsPrintMessage (){
+//     smsbox("This is my sms",1)
+// }
+// setInterval(smsPrintMessage,5000)
